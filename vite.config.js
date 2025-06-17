e8e112fa2ecbd447f892fab408313ce25bea0437
Sun Kim <sunkim@MacBook-Pro.local>
2025-06-16 18:57:14 -0700

    updates

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   base: '/CS601_FinalProject_Kim/',
-  // build: {
-  //   outDir: 'docs',
-  // },
+  build: {
+    outDir: 'dist',
+  },
   plugins: [react()],
 })
